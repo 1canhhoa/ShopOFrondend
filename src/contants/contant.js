c8e882bf07560ce9ee74fca76a99b316e8984a51
1canhhoa <nhamvanhien31102002@gmail.com>
2023-11-20 15:50:23 +0700

    hhh

--- src/contants/contant.js
+++ src/contants/contant.js
@@ -1,3 +1,5 @@
 
 export const server = 'https://shopo-server1.onrender.com'
 export const socket = 'https://shopo-socket-0z7f.onrender.com'
+// export const server = 'http://localhost:4000'
+// export const socket = 'http://localhost:4001'
